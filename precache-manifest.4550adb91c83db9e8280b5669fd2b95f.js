a2a56780aac4a1c91c29c8f2d5788d19388637ce
kuchad-git <57764957+kuchad-git@users.noreply.github.com>
2019-11-16 00:48:13 +0500

    Updates

---
+++ precache-manifest.4550adb91c83db9e8280b5669fd2b95f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f28a653beaaf6ec28daff17be1b6fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cd706474b1cfa085e98",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "b55d14d5a7150bfe3a52",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "5cd706474b1cfa085e98",
+    "url": "/static/js/2.0623109d.chunk.js"
+  },
+  {
+    "revision": "b55d14d5a7150bfe3a52",
+    "url": "/static/js/main.e2b16b3b.chunk.js"
+  },
+  {
+    "revision": "aaa2fd9aed2bb91e1b49",
+    "url": "/static/js/runtime-main.f1c2b8c2.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
